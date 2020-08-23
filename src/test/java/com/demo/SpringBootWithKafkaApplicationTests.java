c3c89e61656ba45a23071aa55d5bf396560c3e7e
SANOGOTECH <sanogotech@yahoo.fr>
2020-08-23 13:08:07 +0000

    Add files via upload

---
+++ src/test/java/com/demo/SpringBootWithKafkaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+public class SpringBootWithKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
